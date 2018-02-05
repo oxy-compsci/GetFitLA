3c564cdd9298c6f2d33410cc112b0c48ba66fd1b
Teagan Mucher <tjsmuch@gmail.com>
2018-02-04 21:27:02 -0800

    Added map api data to manifest

--- GetFitLa/app/src/main/java/team/getfitla/MainActivity.kt
+++ GetFitLa/app/src/main/java/team/getfitla/MainActivity.kt
@@ -2,11 +2,18 @@ package team.getfitla
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Open Map when Map button clicked
+    fun openMap(view: View) {
+        // reminder - look into getting api key restrictions
+
     }
 }
