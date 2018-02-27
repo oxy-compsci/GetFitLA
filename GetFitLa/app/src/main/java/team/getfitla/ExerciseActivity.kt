c84eec56c0b9ddaa27dbf820db9306a5ce92daf5
Teagan Mucher <tjsmuch@gmail.com>
2018-02-26 20:51:21 -0800

    test

--- GetFitLa/app/src/main/java/team/getfitla/ExerciseActivity.kt
+++ GetFitLa/app/src/main/java/team/getfitla/ExerciseActivity.kt
@@ -1,8 +1,8 @@
 package team.getfitla
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 
 class ExerciseActivity : AppCompatActivity() {
@@ -19,6 +19,7 @@ class ExerciseActivity : AppCompatActivity() {
         val noEquipIntent = Intent(this, NoEquipmentActivity::class.java)
         startActivity(noEquipIntent)
     }
+    
 
 }
 
