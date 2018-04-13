d3d484f2b7c62a6e3605942c6408340a89d09363
Teagan Mucher <tjsmuch@gmail.com>
2018-04-12 17:25:24 -0700

    Added functionality to see if URL is reachable
    Throws a 301 code despite pasting url into browser takes you to kml file download?

--- app/src/test/java/practicum/getfitla_v3/MapsActivityTest.java
+++ app/src/test/java/practicum/getfitla_v3/MapsActivityTest.java
@@ -2,6 +2,9 @@ package practicum.getfitla_v3;
 
 import org.junit.Test;
 
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,7 +21,26 @@ public class MapsActivityTest {
         Pattern urlPattern = Pattern.compile(regex);
         Matcher matcher = urlPattern.matcher(mUrl);
 
-        System.out.println(matcher.matches());
+        System.out.println("Valid URL format: " + matcher.matches());
+
+        URL url = null;
+        int code = 0;
+        try {
+            url = new URL(mUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            code = connection.getResponseCode();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+
+        if(code == 200) {
+            System.out.println("Reachable URL");
+        } else {
+            System.out.println("URL not reachable, code: " + code);
+        }
+
 
     }
 
