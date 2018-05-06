211f60c7c333f3e7cc16a983cf5cc0723f5a5da6
Daniel Huth <dhuth@oxy.edu>
2018-05-06 13:37:39 -0700

    removed merge conflict lines

--- app/src/main/java/practicum/getfitla_v3/AsyncCallback.java
+++ app/src/main/java/practicum/getfitla_v3/AsyncCallback.java
@@ -3,11 +3,6 @@ package practicum.getfitla_v3;
 import org.json.JSONObject;
 
 /**
-<<<<<<< HEAD
-=======
- * Created by dhuth on 1/14/2015.
- *
->>>>>>> master
  * Interface for the callback function of an asynchronous task.
  * This allows different post-processing tasks to be determined on a per-instance basis.
  */
